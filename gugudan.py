8b39411bbd8abe92408977c53a82e28b8acd5384
junhyuk0801 <junhyuk0801@gmail.com>
2021-10-08 10:29:51 +0900

    changed nine-by-nine

--- gugudan.py
+++ gugudan.py
@@ -1,5 +1,6 @@
 print('구구단 출력')
-for i in range(1,9):
+# changed nine-by-nine
+for i in range(1,10):
     print('--- ' + str(i) + ' 단---')
     for j in range(1,10):
         print(i, 'X', j,'=',i*j)
